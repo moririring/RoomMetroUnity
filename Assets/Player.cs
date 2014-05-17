1a107d47263a0a187a82f5a1c056a864c836bfde
moririring <moriri.ring10@gmail.com>
2014-05-17 23:10:58 +0900

    弾を撃つ

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,34 +1,58 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Linq;
+using UnityEngine;
 
 public class Player : MonoBehaviour {
 
     public float _speed = 1.0f;
     public float _force = 1.0f;
-    public bool jump;
-    // Use this for initialization
+    public float _velocity = 30.0f;
+    [Range(4,360)]public int  _hanas = 20;
+    public GameObject _bulletPrefab;
+
+    private bool _jump;
 	void Start () {
 	}
-
-	
-	// Update is called once per frame
 	void Update ()
 	{
         var x = Input.GetAxis("Horizontal");
         var z = Input.GetAxis("Vertical");
         transform.Translate(new Vector3(x, 0, z) * _speed);
-        if (!jump && Input.GetButtonDown("Jump"))
+        if (!_jump && Input.GetButtonDown("Jump"))
         {
-            jump = true;
+            _jump = true;
             rigidbody.AddForce(transform.up * _force, ForceMode.Impulse);
         }
-
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                var bullet = Instantiate(_bulletPrefab, transform.position, transform.rotation) as GameObject;
+                bullet.rigidbody.velocity = (hit.point - new Vector3(transform.position.x, 0, transform.position.z)).normalized * _velocity;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            foreach (var hana in Enumerable.Range(0, _hanas).Select(r => r * (360 / _hanas) * Mathf.Deg2Rad))
+            {
+                var bullet = Instantiate(_bulletPrefab, transform.position, transform.rotation) as GameObject;
+                bullet.rigidbody.velocity = new Vector3(Mathf.Cos(hana), 0, Mathf.Sin(hana)).normalized * _velocity;
+                
+            }
+            
+//            for (int i = 0; i < 360; i += 20)
+//            {
+//                var rad = i * Mathf.Deg2Rad;
+//                var bullet = Instantiate(_bulletPrefab, transform.position, transform.rotation) as GameObject;
+//                bullet.rigidbody.velocity = new Vector3(Mathf.Cos(rad), 0, Mathf.Sin(rad)).normalized * _velocity;
+//            }
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Floor")
         {
-            jump = false;
+            _jump = false;
         }
     }
 
