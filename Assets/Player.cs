45a320e5f9bc7378e36ab1cfb3769d56f065c58c
moririring <moriri.ring10@gmail.com>
2014-05-17 20:45:21 +0900

    移動の基本

---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        if (Input.GetKey("up"))
+            transform.Translate(0, 0, 0.1f);
+        else if (Input.GetKey("down"))
+            transform.Translate(0, 0, -0.1f);
+        else if (Input.GetKey("left"))
+            transform.Translate(-0.1f, 0, 0);
+        else if (Input.GetKey("right"))
+            transform.Translate(0.1f, 0, 0);
+    }
+}
